37d0ca93eee5eb2b0c142d571a3bae19782cfa2d
Spoorthi S <126961081+Spoooo8@users.noreply.github.com>
2025-06-19 21:55:34 +0530

    added config server

---
+++ configserver/src/test/java/com/unilink/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unilink.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
